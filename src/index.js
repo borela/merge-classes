51c143b5fe889672b4a9a8ae3e5ab9bf794858d5
Alexandre Borela <alexandre@borela.tech>
2016-12-04 03:25:05 -0200

    Add the function that merges CSS classes.

---
+++ src/index.js
@@ -0,0 +1,87 @@
+// Licensed under the Apache License, Version 2.0 (the “License”); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+function getClassesFromArray(targetArray) {
+  let result = [];
+
+  for (let item of targetArray) {
+    let itemType = typeof item;
+
+    // Normal CSS class.
+    if (parameterType === 'string') {
+      result.push(parameter);
+      continue;
+    }
+
+    // Module.
+    if (parameterType === 'object') {
+      result = result.concat(
+        getClassesFromModule(parameter)
+      );
+      continue;
+    }
+
+    // Array of normal CSS classes or modules.
+    if (parameterType === 'string') {
+      result = result.concat(
+        getClassesFromArray(parameter)
+      );
+      continue;
+    }
+  }
+
+  return result;
+}
+
+function getClassesFromModule(module) {
+  let result = [];
+
+  for (let property in module) {
+    result.push(module[property]);
+  }
+
+  return result;
+}
+
+export function mergeClasses(...parameters) {
+  let result = [];
+
+  for (let parameter of parameters) {
+    let parameterType = typeof parameter;
+
+    // Normal CSS class.
+    if (parameterType === 'string') {
+      result.push(parameter);
+      continue;
+    }
+
+    // Module.
+    if (parameterType === 'object') {
+      result = result.concat(
+        getClassesFromModule(parameter)
+      );
+      continue;
+    }
+
+    // Array of normal CSS classes or modules.
+    if (parameterType === 'string') {
+      result = result.concat(
+        getClassesFromArray(parameter)
+      );
+      continue;
+    }
+  }
+
+  return result.join(' ');
+}
+
+export default mergeClasses;
