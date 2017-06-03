b47d69aeefb4dfd49bc21ef7dc7724615ba5fa7a
Alexandre Borela <alexandre@borela.tech>
2017-06-03 17:33:26 -0300

    Simplify tests

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -11,7 +11,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-import mergeClasses from '../../src'
+import mergeClasses from '..'
 
 const SIMPLE_CLASS_A = 'simple-a'
 const SIMPLE_CLASS_B = 'simple-b'
